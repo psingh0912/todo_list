f47977f819562ffc8d07640e06e34ef480b50e6e
Pawan Singh <psingh0912@gmail.com>
2016-03-10 13:26:46 +0530

    mod2 rq13

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
     has_many :todo_items, through: :todo_lists, source: :todo_items
 
     validates :username, presence: :true
+
+    def get_completed_count
+        self.todo_items.where(completed: true).count
+    end
 end
