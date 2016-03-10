11fae32d7428a38d9cee7c9ec29ace69a8d902f2
Pawan Singh <psingh0912@gmail.com>
2016-03-10 11:01:45 +0530

    mod2 rq05

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160310052511) do
+ActiveRecord::Schema.define(version: 20160310053040) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "gender"
@@ -30,7 +30,10 @@ ActiveRecord::Schema.define(version: 20160310052511) do
     t.date     "list_due_date"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.integer  "user_id"
   end
+
+  add_index "todo_lists", ["user_id"], name: "index_todo_lists_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
