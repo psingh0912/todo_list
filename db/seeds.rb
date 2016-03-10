b9198b35635ccc850ce2d9842fe22b5e7073f2f5
Pawan Singh <psingh0912@gmail.com>
2016-03-10 13:39:17 +0530

    Module 4 Starts. start_spec completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+today = Date.today
+two_days_ago = Date.today - 2.days
+three_days_ago = Date.today - 3.days
+dates = [today, two_days_ago, three_days_ago]
 
 User.destroy_all
-Profile.destroy_all
 TodoList.destroy_all
-TodoItem.destroy_all
 
-users = [
-    ["Carly Fiorina", 1954, "female"],
-    ["Donald Trump", 1946, "male"],
-    ["Ben Carson", 1951, "male"],
-    ["Hillary Clinton", 1947, "female"]
-]
+100.times { |index| TodoList.create! list_name: "List #{index}", list_due_date: dates.sample }
 
-
-users.each do |u|
-    fname, lname = u[0].split[0], u[0].split[1]
-
-    user = User.create(username: lname, password_digest: fname.downcase.reverse)
-    user.profile = Profile.create(gender: u[2], birth_year: u[1], first_name: fname, last_name: lname)
-
-    tl = user.todo_lists.create(list_name: "The #{lname} List", list_due_date: Date.today+1.year)
-
-    5.times do |i|
-        tl.todo_items.create(due_date: Date.today+1.year, title: "Item #{i+1}", description: "This is task #{i+1}", completed: false)
-    end
+TodoList.all.each do |list|
+  list.todo_items.create! [
+    { title: "Task 1", due_date: dates.sample, description: "very important task TEST", completed: false },
+    { title: "Task 2", due_date: dates.sample, description: "do something else TEST", completed: true},
+    { title: "Task 3", due_date: dates.sample, description: "learn Action Pack TEST", completed: true}
+  ]
 end
 
+users = User.create! [
+  { username: "jim", password: "abc123" },
+  { username: "rich", password: "123abc" }
+]
+
+TodoList.all.each do |list|
+  list.user = users.sample
+  list.save!
+end
